3c03c6ddfe10ae8e0674437ce6b8c9ac0d9f8c3a
tuanguyen <tuanguyen@axon.com>
2023-01-05 10:43:25 +0700

    update

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'vi',
     locales: ['vi', 'en'],
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
   },
 }
